6755f15ae75926d4e04f8ccf3cc481d8fcf237bd
Anand prabhakar <anandprabhakar0507@gmail.com>
2018-10-28 10:29:06 -0700

    Create input multiply.py

---
+++ input multiply.py
@@ -0,0 +1,12 @@
+#THIS SCRIPT TAKES INPUT OF NUMBERS and MULTIPLY THEM
+
+
+# Store input numbers
+A = input('')
+D = input('')
+print('YOU JUST ENTERED THE VALUE OF A={0} and D={1}'.format(A, D))
+# multiply two numbers
+into = float(A) * float(D)
+
+# Display the product
+print('The PRODUCT of A and D or AD or A*D is {2}'.format(A, D, into))
